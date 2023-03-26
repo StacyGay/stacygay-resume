b095a623c164df27a831deb03b90964305b48533
Stacy Gay <stacygay@Stacys-MacBook-Pro.local>
2023-03-26 19:29:08 -0400

    initial animations

--- src/experience/job-item.tsx
+++ src/experience/job-item.tsx
@@ -1,14 +1,36 @@
-import { ReactElement } from 'react';
+import { motion, useAnimation } from 'framer-motion';
+import { ReactElement, useEffect } from 'react';
+import { useInView } from 'react-intersection-observer';
 import { Job } from './job-service';
 
 export interface JobProps {
     job: Job;
 }
 
+const boxVariant = {
+    visible: { opacity: 1, scale: 1, x: 0, transition: { duration: 0.5 }},
+    hidden: { opacity: 0, scale: 0, x: 200 },
+};
+
 export function JobItem({ job }: JobProps): ReactElement {
-    console.log(job);
+    const control = useAnimation();
+    const [ref, inView] = useInView();
+
+    useEffect(() => {
+        if (inView) {
+            control.start("visible");
+        } else {
+            control.start("hidden");
+        }
+    }, [control, inView]);
+
     return (
-        <div className="card w-full mx-2 my-5 p-4 bg-base-100 shadow-xl">
+        <motion.div 
+            className="card w-full mx-2 my-5 p-4 bg-base-100 shadow-xl slide-in"
+            ref={ref}
+            variants={boxVariant}
+            initial="hidden"
+            animate={control}>
             <div className="card-body text-left">
                 <h3 className="card-title">
                     {job.title}
@@ -20,6 +42,6 @@ export function JobItem({ job }: JobProps): ReactElement {
                     {job?.items.map(i => <li>{i}</li>)}
                 </ul>
             </div>
-        </div>
+        </motion.div>
     );
 }
